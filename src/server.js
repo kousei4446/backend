a2600902305014a79e2083c8105507f182588b33
kousei4446 <e1922022@oit.ac.jp>
2025-05-25 23:01:05 +0900

    初期化スクリプトの実行条件を追加

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const { port } = require('./config');
 const logger   = require('./config/logger');
 const app      = require('./app');
 
+if (process.env.RUN_INIT === 'true') {
+  require('../initDb');  // ← パスは backend/initDb.js に合わせて
+}
+
 // Swagger セットアップを追加！
 require('./docs/swagger')(app);
 
