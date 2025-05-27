f91e4bce68b116bfac24e35d92f0c9b939303845
kousei4446 <e1922022@oit.ac.jp>
2025-05-27 11:33:35 +0900

    初期化スクリプトの実行条件をコメントアウトから有効に変更

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const { port } = require('./config');
 const logger = require('./config/logger');
 const app = require('./app');
 
-// if (process.env.RUN_INIT === 'true') {
-//   require('../initDb');  // ← パスは backend/initDb.js に合わせて
-// }
+if (process.env.RUN_INIT === 'true') {
+  require('../initDb');  // ← パスは backend/initDb.js に合わせて
+}
 
 // Swagger セットアップを追加！
 require('./docs/swagger')(app);
